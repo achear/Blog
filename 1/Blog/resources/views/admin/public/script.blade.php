03228153583e1580f139c31eacb13166aba25c14
lamp-adong <3424246568@qq.com>
2018-03-09 10:20:32 +0800

    del多余文件

--- 1/Blog/resources/views/admin/public/script.blade.php
+++
@@ -1,3 +0,0 @@
-<script type="text/javascript" src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
-<script src="/Admin/lib/layui/layui.js" charset="utf-8"></script>
-<script type="text/javascript" src="/Admin/js/xadmin.js"></script>
